de64f9328087665c32723da1ed8634cfd72eea90
keya-moradi <keya.moradi@gmail.com>
2024-08-10 01:45:56 -0700

    setup for HTML dives + CSS grids and styling 2.0 + Some JS constants

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// Get references to important elements
+const welcomeScreen = document.getElementById('welcome-screen');
+const gameBoard = document.getElementById('game-board');
+const gridContainer = document.getElementById('grid-container');
+const scoreDisplay = document.getElementById('score');
+const restartButton = document.getElementById('restart-button');
+const   
+ instructionsButton = document.getElementById('show-instructions');
+const gameInstructions = document.getElementById('game-instructions');
+
+// Set up game variables
+let grid = []; // 2D array to represent the game board
+let score = 0;
+
